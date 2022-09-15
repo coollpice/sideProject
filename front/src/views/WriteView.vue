60aabbf556ab7b96769187e7912780db358d2664
coollpice <coollpice@nvaer.com>
2022-09-15 18:04:30 +0900

    Vue.js 설치 - 화면구성.

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  const count = ref(0);
+</script>
+
+<template>
+  <p>테스트</p>
+  <button @click="count += 1">누르세요</button>
+  <p>{{count}}</p>
+</template>
+
+<style>
+
+</style>
