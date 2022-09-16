2fd8067212715ec098b0cd2e0b69dcc8c438407c
coollpice <coollpice@nvaer.com>
2022-09-16 17:21:56 +0900

    게시글 삭제기능 추가.

--- front/src/views/ReadView.vue
+++ front/src/views/ReadView.vue
@@ -28,12 +28,23 @@ const moveToEdit = () => {
   router.push({name : "edit", params : { postId : props.postId}});
 };
 
+const deletePosts = () => {
+  if(confirm("삭제하시겠습니까?")){
+    axios.delete(`/api/posts/${props.postId}`).then(() => {
+      router.replace({name : "home"})
+    });
+  }
+};
+
 </script>
 
 <template>
   <h2>{{post.title}}</h2>
   <div>{{post.content}}</div>
-  <div><el-button type="warning" @click="moveToEdit()">수정</el-button></div>
+  <div>
+    <el-button type="warning" @click="moveToEdit()">수정</el-button>
+    <el-button type="danger" @click="deletePosts()">삭제</el-button>
+  </div>
 </template>
 
 <style>
